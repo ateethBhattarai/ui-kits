c68200bd9c68601264d46362bf117f6ffd520898
ateethBhattarai <ateethbhattarai02@gmail.com>
2023-12-09 08:30:16 +0545

    initial setup

---
+++ src/app/page.tsx
@@ -0,0 +1,76 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import { Button, buttonVariants } from "@/components/ui/button";
+import { ArrowDownToLine, CheckCircle, Leaf } from "lucide-react";
+import Link from "next/link";
+
+const perks = [
+  {
+    name: "Instant Delivery",
+    icon: ArrowDownToLine,
+    desc: "Get your assets delivered to your email in seconds.",
+  },
+  {
+    name: "Guaranteed Quality",
+    icon: CheckCircle,
+    desc: "Every assets on our platform are verified by our team.",
+  },
+  {
+    name: "For the planet",
+    icon: Leaf,
+    desc: "We've pledged one percent of sale to the preservation and restoration of natural monuments.",
+  },
+];
+
+export default function Home() {
+  return (
+    <>
+      <MaxWidthWrapper>
+        <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
+          <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+            Your marketplace for high-quality{" "}
+            <span className="text-blue-600">digital assets</span>.
+          </h1>
+          <p className="mt-6 text-lg max-w-prose text-muted-foreground">
+            Welcome to UI KITS. Every assets on our platform are verified by our
+            team to ensure our highest quality standards.
+          </p>
+          <div className="flex flex-col sm:flex-row gap-4 mt-6">
+            <Link href={"/products"} className={buttonVariants()}>
+              Browse Trending
+            </Link>
+            <Button className="capitalize" variant="ghost">
+              Our Quality promise &rarr;
+            </Button>
+          </div>
+        </div>
+      </MaxWidthWrapper>
+
+      <section className="border-t border-gray-200 bg-gray-50">
+        <MaxWidthWrapper className="py-20">
+          <div className="grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-3 lg:gap-x-8 lg:gap-y-0">
+            {perks.map((perkItem) => (
+              <div
+                key={perkItem.name}
+                className="text-center md:flex md:items-center md:text-left lg:block lg:text-center"
+              >
+                <div className="md:flex-shrink-0 flex justify-center">
+                  <div className="h-16 w-16 flex items-center justify-center rounded-full bg-blue-100 text-blue-900">
+                    {<perkItem.icon className="w-1/3 h-1/3" />}
+                  </div>
+                </div>
+                <div className="mt-6 md:ml-4 md:mt-0 lg:ml-0 lg:mt-6">
+                  <h3 className="text-base font-medium text-gray-900">
+                    {perkItem.name}
+                  </h3>
+                  <p className="mt-3 text-sm text-muted-foreground">
+                    {perkItem.desc}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </MaxWidthWrapper>
+      </section>
+    </>
+  );
+}
---
+++ src/config/index.ts
@@ -0,0 +1,13 @@
+export const PRODUCT_CATEGORIES = [
+  {
+    label: "UI KITS",
+    value: "ui-kits" as const,
+    feature: [
+      {
+        name: "Editor picks",
+        href: "#",
+        imageSrc: "/nav/ui-kits/mixed.jpg",
+      },
+    ],
+  },
+];
