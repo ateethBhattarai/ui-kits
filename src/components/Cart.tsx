cb0f09706515a2dac215dcd9c462cc2066e2e1a4
ateethBhattarai <ateeth.bhattarai02@gmail.com>
2023-12-09 10:50:13 +0545

    cart ui/ux if items avilable in cart

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -4,12 +4,19 @@ import { ShoppingCart } from "lucide-react";
 import {
   Sheet,
   SheetContent,
+  SheetFooter,
   SheetHeader,
   SheetTitle,
   SheetTrigger,
 } from "./ui/sheet";
+import { Separator } from "./ui/separator";
+import { formatPrice } from "@/lib/utils";
+import Link from "next/link";
+import { buttonVariants } from "./ui/button";
 
 const Cart = () => {
+  const itemCount = 1;
+  const fee = 10;
   return (
     <Sheet>
       <SheetTrigger className="group -m-2 flex items-center p-2">
@@ -22,6 +29,37 @@ const Cart = () => {
         <SheetHeader className="space-y-2.5 pr-6">
           <SheetTitle>Cart (0)</SheetTitle>
         </SheetHeader>
+        {itemCount > 0 ? (
+          <>
+            <div className="flex w-full flex-col pr-6">
+              Cart Items
+              {/* TODO: Cart Items Logic */}
+            </div>
+            <div className="space-y-4 pr-6">
+              <Separator />
+              <div className="space-y-1.5 pr-6">
+                <div className="flex">
+                  <span className="flex-1">Shipping</span>
+                  <span>Free</span>
+                </div>
+                <div className="flex">
+                  <span className="flex-1">Transaction Fee</span>
+                  <span>{formatPrice(fee)}</span>
+                </div>
+              </div>
+              <SheetFooter>
+                <Link
+                  href={"/cart"}
+                  className={buttonVariants({ className: "w-full" })}
+                >
+                  Continue to Checkout
+                </Link>
+              </SheetFooter>
+            </div>
+          </>
+        ) : (
+          <div></div>
+        )}
       </SheetContent>
     </Sheet>
   );
