19b2a9ea9fbb5e308fd79462aa67883c3e6646f4
ateethBhattarai <ateeth.bhattarai02@gmail.com>
2023-12-09 11:00:56 +0545

    cart static ui/ux

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,6 +1,11 @@
 "use client";
 
+import { formatPrice } from "@/lib/utils";
 import { ShoppingCart } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { buttonVariants } from "./ui/button";
+import { Separator } from "./ui/separator";
 import {
   Sheet,
   SheetContent,
@@ -9,13 +14,9 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "./ui/sheet";
-import { Separator } from "./ui/separator";
-import { formatPrice } from "@/lib/utils";
-import Link from "next/link";
-import { buttonVariants } from "./ui/button";
 
 const Cart = () => {
-  const itemCount = 1;
+  const itemCount = 0;
   const fee = 10;
   return (
     <Sheet>
@@ -58,7 +59,28 @@ const Cart = () => {
             </div>
           </>
         ) : (
-          <div></div>
+          <div className="flex h-full flex-col items-center justify-center space-y-1">
+            <div className="relative mb-4 h-60 w-60 text-muted-foreground">
+              <Image
+                src="/hippo-empty-cart.png"
+                alt="empty shopping cart"
+                fill
+              />
+            </div>
+            <div className="text-xl font-semibold">Your Cart is empty</div>
+            <SheetTrigger>
+              <Link
+                href={"/products"}
+                className={buttonVariants({
+                  variant: "link",
+                  size: "sm",
+                  className: "text-sm text-muted-foreground",
+                })}
+              >
+                Add items to your cart to checkout
+              </Link>
+            </SheetTrigger>
+          </div>
         )}
       </SheetContent>
     </Sheet>
